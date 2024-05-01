f1e516ff5bc402d6b57acedab5d7bda835833356
MollikaComputer Provider <mollikacomputer3@gmail.com>
2024-05-01 19:53:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h2 className='text-3xl'>Homne Page</h2>
+    <div className="card w-96 bg-base-100 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+  </div>
+  <figure><img  src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+</div>
+
+    </>
+  )
+}
+
+export default App
